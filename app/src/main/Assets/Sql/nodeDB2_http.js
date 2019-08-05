64f18c8f7664f59e3c706fae313dd5861a69efc9
Andre Sa <andreg_sa@hotmail.com>
2019-08-04 23:22:27 -0700

    andre final

--- app/src/main/Assets/Sql/nodeDB2_http.js
+++ app/src/main/Assets/Sql/nodeDB2_http.js
@@ -90,7 +90,7 @@ app.post('/GET_ONE', function (req, res) {
     console.log(req);
 
 
-    let qString = "select t1.*, t2.*, t3.* from Orders t1 inner join Order_Detail t2 on t1.OrderID = t2.OrderID inner join Product t3 on t2.ProductID = t3.ProductID where t1.UserID='" + req.body.where_key + "'";
+    let qString = "select t1.*, t2.*, t3.* from Orders t1 inner join Order_Detail t2 on t1.OrderID = t2.OrderID inner join Store t3 on t1.StoreID = t3.StoreID where t1.UserID='" + req.body.where_key + "'";
 
     connection.query(qString, function(err, rows, fields) {
     if (!err) {
@@ -235,52 +235,90 @@ var words = str.split(',');
 
 var address="";
 
-for(var x=5;x<words.length;x++){
+for(var x=4;x<words.length;x++){
 	address+=words[x];
 }
 
 
 
-  console.log(address);
-
-for(var x=0;x<words.length;x++){
-console.log(words[x]);
-}
-
-
     let qString = "INSERT INTO Orders (StoreID,UserID,Total,Total_Items,DeliveryAdd) VALUES (" +
-    words[0]+",'"+words[1]+"',"+words[3] +","+words[4] +","+ "'"+address+"'" +")" ;
+    words[0]+",'"+words[1]+"',"+words[2] +","+words[3] +",'"+ words[4]+"')" ;
 
-    console.log(qString);
+   // console.log(qString);
 
-    connection.query(qString, function(err, rows, fields) {
+  let OrderID;
+
+
+   connection.query(qString, function(err, rows, fields) {
     if (!err) {
         console.log("Displaying all the rows");
 
+       OrderID = rows.insertId;
 
-       console.log(rows.insertId);
+
+       console.log("ORDER ID"+ OrderID );
+
+
     }
     else
-        console.log('Error while performing Query.');
+        console.log('Error while performing Query. 1');
     });
 
 
-    let qLastResult= "SELECT LAST_INSERT_ID()";
+    setTimeout(test, 5000);
 
-     connection.query(qLastResult, function(err, rows, fields) {
+    function test(){
+
+    console.log("TEST ORDERID :" + OrderID);
+
+    //aqui comeca a segunda parte
+
+    var str = req.body.where_value2;
+
+    var words = str.split(';');
+
+    var result="";
+
+    var OrderIDSTR = OrderID+"";
+
+    for(var x=0;x<words.length;x++){
+      if(x<(words.length-1)){
+
+        words[x] =words[x].substring(0,1)+OrderIDSTR+","+words[x].substring(1,words[x].length);
+        console.log("WORDS: "+words[x]);
+
+          result += words[x];
+
+
+         }
+
+      var results = result.substring(0,(result.length-1));
+      console.log(results);
+
+      }
+
+
+
+
+     let qString2 = "INSERT INTO Order_Detail (OrderID,ProductID,Quantity,SubTotal) VALUES "+ results;
+
+
+
+    console.log(qString2);
+
+    connection.query(qString2, function(err, rows, fields) {
     if (!err) {
-        console.log("LAST INSERT ID " + rows);
+        console.log("Displaying all the rows");
 
-
+        res.send(JSON.stringify(rows));
 
 
     }
     else
-        console.log('Error while performing Query.');
+        console.log('Error while performing Query. 2');
     });
 
-
-
+    }
 
     
  });
